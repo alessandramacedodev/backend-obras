c5f1d517b6c7ca1d164a59a9eefdb9d39ecfbd6a
Alessandra Macedo <alessandramacedo.dev@gmail.com>
2025-06-14 20:30:02 -0300

    Fiscalizacao.js

--- models/Fiscalizacao.js
+++ models/Fiscalizacao.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
 const FiscalizacaoSchema = new mongoose.Schema({
-  data: Date,
-  status: String,
+  data:  { type: Date, required: true },
+  status: { type: String, required: true },
   observacoes: String,
   localizacao: {
     lat: Number,
     long: Number
   },
   foto: String,
-  obra: { type: mongoose.Schema.Types.ObjectId, ref: 'Obra' }
+  obra: { type: mongoose.Schema.Types.ObjectId, ref: 'Obra', required: true }
 });
 
 module.exports = mongoose.model('Fiscalizacao', FiscalizacaoSchema);
